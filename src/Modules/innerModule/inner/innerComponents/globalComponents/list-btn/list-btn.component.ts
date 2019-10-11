b4ac04ee6485998fe8f81a33cdb29cb30f4e43cc
Decory <decory@breezemaxweb.com>
2019-10-11 17:42:12 -0400

    no message

---
+++ src/Modules/innerModule/inner/innerComponents/globalComponents/list-btn/list-btn.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-btn',
+  templateUrl: './list-btn.component.html',
+  styleUrls: ['./list-btn.component.scss']
+})
+export class ListBtnComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
