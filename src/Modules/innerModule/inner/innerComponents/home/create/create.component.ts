76f06df854e7e8f570dc70e56525ec4b0caf42d8
breezemax <you@example.com>
2019-10-18 18:43:18 -0400

    dropt stilll not working

--- src/Modules/innerModule/inner/innerComponents/home/create/create.component.ts
+++ src/Modules/innerModule/inner/innerComponents/home/create/create.component.ts
@@ -14,21 +14,31 @@ export class CreateComponent implements OnInit {
   ErrorMessage;
   ListInfo: any[];
   modalid;
-  cName:string
+  cName: string;
+  CID: string;
 
   dropdownList = [];
+  dropdownList2 = [];
   selectedItems_: any;
-  dropdownSettings:IDropdownSettings  = {};
+  dropdownSettings: IDropdownSettings = {};
+  dropdownSettings2: IDropdownSettings = {};
+
+  selectedItems = [];
+  selectedItems1 = [];
 
   registerForm: FormGroup; //new
   submitted = false; //new
 
+  setOptions;
+
   @Input() formsdataforpost = { client_name: '', client_email: '' };
 
-  constructor(private data: DataService, private spinner: NgxSpinnerService, private toastr: ToastrService,private formBuilder: FormBuilder) { }
+  constructor(private data: DataService, private spinner: NgxSpinnerService, private toastr: ToastrService, private formBuilder: FormBuilder) { }
   formsdata = { client_name: '', client_email: '', id: '' };
 
-  selecteddata = { id: '', client_name: '', client_email: '' }
+  selecteddata = { id: '', client_name: '', client_email: '' };
+  selecteddataid = { id: '' };
+
   modalName;
   ngOnInit() {
     this.spinner.show();
@@ -36,14 +46,13 @@ export class CreateComponent implements OnInit {
     this.getleads();
     this.registerForm = this.formBuilder.group({ //new
       client_name: ['', Validators.required],
-      client_email: ['', [Validators.required,Validators.email]],
-     });
+      client_email: ['', [Validators.required, Validators.email]],
+    });
 
   }
 
-  initialize()
-  {
-   
+  initialize() {
+
     this.dropdownSettings = {
       singleSelection: false,
       idField: 'id',
@@ -54,11 +63,45 @@ export class CreateComponent implements OnInit {
     };
   }
 
-  
+
+
+  initializeProducts() {
+
+    this.dropdownSettings2 = {
+      singleSelection: false,
+      idField: 'id',
+      textField: 'product_name',
+      selectAllText: 'Select All',
+      unSelectAllText: 'UnSelect All',
+      itemsShowLimit: 3,
+      allowSearchFilter: true
+    };
+  }
+
+
+  onItemDeselect(item: any) {
+
+    // Updating service file
+    this.setOptions.deleteMultiSelectOptions(item.item_id, item.item_text)
+
+  }
+
+  onItemSelect1(item: any) {
+    this.setOptions.setMultiSelectOptions(item.item_id, item.item_text)
+    console.log(item);
+  }
+
+  onSelectAll(items: any) {
+    this.selectedItems1.push(items)
+    console.log(this.selectedItems1);
+
+  }
+
+
 
   get fval() {
     return this.registerForm.controls; //new 
-    }
+  }
 
 
   getleads() {
@@ -77,7 +120,7 @@ export class CreateComponent implements OnInit {
 
   //lead Modal injection
   callNewModal(modaldata) {
-    
+
     this.modalName = modaldata.client_name
     this.selecteddata.id = modaldata.id
     this.selecteddata.client_name = modaldata.client_name
@@ -114,8 +157,7 @@ export class CreateComponent implements OnInit {
         this.getleads();
         this.spinner.hide();
       }
-      else
-      {
+      else {
         this.spinner.hide();
         this.ErrorMessage = "Error : " + result.message;
         this.showderror(this.ErrorMessage);
@@ -152,17 +194,19 @@ export class CreateComponent implements OnInit {
 
   // Create Proposal button
   CreateProposal(companyName) {
-    
+
     (document.querySelector('.content_Wrapper') as HTMLElement).style.display = 'none';
     (document.querySelector('.customPKG_page_Edit') as HTMLElement).style.display = 'block';
     this.cName = companyName;
+    this.selecteddataid.id = this.selectedItems_;
+    this.getProductlists();
     console.log(companyName)
   }
 
   //create lead
   leadInformation() {
 
-    
+
     this.spinner.show();
     this.formsdata.client_email = this.formsdataforpost.client_email;
     this.formsdata.client_name = this.formsdataforpost.client_name;
@@ -198,7 +242,7 @@ export class CreateComponent implements OnInit {
 
 
   getdropdown() {
- 
+
     this.data.Getdropdwonlist().subscribe((result) => {
       //console.log(result);
       if (result) {
@@ -211,12 +255,30 @@ export class CreateComponent implements OnInit {
     });
   }
 
+
+
+  getProductlists() {
+console.log(this.selecteddataid);
+    this.data.GetProductlist(this.selecteddataid).subscribe((result) => {
+      console.log(result);
+      if (result) {
+        this.dropdownList2 = result.data
+        this.spinner.hide();
+      }
+    }, (err) => {
+      this.spinner.hide();
+      this.ErrorMessage = "Error : " + err + " .Please tell admin about this error";
+    });
+
+  }
+
   onItemSelect(item: any) {
     this.selectedItems_ = item.id
+
     console.log(this.selectedItems_);
   }
 
-  
+
 
   showSuccess() {
     this.toastr.success('Lead Created', 'Success');
